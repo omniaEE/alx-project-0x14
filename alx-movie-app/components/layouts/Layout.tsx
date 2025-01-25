52db27cec1fbf187f4146da7edc882bf3c984db9
Omnia <omniaeid314@gmail.com>
2025-01-25 18:25:17 +0200

    the layout

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,1 +1,15 @@
-const Layout = ({ children }) => { return <div>{children}</div>; }; export default Layout;
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
